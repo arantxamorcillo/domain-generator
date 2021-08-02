5707d61e035c522a62f3536b7ec83cf364150683
Arantxa Morcillo <arantxa.morcillo@gmail.com>
2021-08-02 18:43:53 +0200

    Added string

--- domain.js
+++ domain.js
@@ -1,3 +1,6 @@
+
+
+
 let pronoun = ['the','our','last','first'];
 let adj = ['great', 'big','of','call' ];
 let noun = ['jogger','me','racoon','us'];
@@ -15,13 +18,13 @@ function domainGenerator (part1, part2, part3, part4){
                 for (let l=0; l<part4.length; l++){
                     let domain =''
                     if ( part4.includes(part3[k])){
-                        domain= part1[i] + part2[j] + "." + part3[k]
+                        domain= 'This is a Domain Hack ' + part1[i] + part2[j] + "." + part3[k]
                         if (listDomains.includes(domain)){}
                         else (listDomains.push(domain))
                     }
                     else{
-                        //domain =part1[i] + part2[j] + part3[k] + "." + part4[l]
-                        //listDomains.push(domain)
+                        domain =part1[i] + part2[j] + part3[k] + "." + part4[l]
+                        listDomains.push(domain)
                     }
                 }
             }
