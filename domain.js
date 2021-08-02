99f15665371ec5153356dce65714008ad09312f5
Arantxa Morcillo <arantxa.morcillo@gmail.com>
2021-08-02 12:35:33 +0200

    domainhack only once

--- domain.js
+++ domain.js
@@ -4,7 +4,8 @@ let noun = ['jogger','me','racoon','us'];
 let ext = ['com', 'us','uk','es','me']
 
 function domainGenerator (part1, part2, part3, part4){
-   
+    let listDomains = []
+
     for (let i=0; i<part1.length; i++){
         
         for (let j=0; j<part2.length; j++){
@@ -12,17 +13,20 @@ function domainGenerator (part1, part2, part3, part4){
             for (let k=0; k<part3.length; k++){
                 
                 for (let l=0; l<part4.length; l++){
+                    let domain =''
                     if ( part4.includes(part3[k])){
-                        domain = part1[i] + part2[j] + "." + part3[k]
-                        console.log ("DOMAIN HACK " + domain)
+                        domain= part1[i] + part2[j] + "." + part3[k]
+                        if (listDomains.includes(domain)){}
+                        else (listDomains.push(domain))
                     }
                     else{
-                        domain =part1[i] + part2[j] + part3[k] + "." + part4[l]
-                        console.log(domain)
+                        //domain =part1[i] + part2[j] + part3[k] + "." + part4[l]
+                        //listDomains.push(domain)
                     }
                 }
             }
         }    
     }
+    return listDomains
 }
-domainGenerator(pronoun, adj, noun,ext)
+console.log (domainGenerator(pronoun, adj, noun,ext))
